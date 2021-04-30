cfa0e9cab803f96cd0b24d57c2605d50f184158d
Wenxuan Li <71944101+WenxuanLiGitHub@users.noreply.github.com>
2021-04-30 19:33:35 +0200

    Implement post message function with axios
    - Also update ZoneStatistique

--- src/components/ListeMessage.js
+++ src/components/ListeMessage.js
@@ -4,10 +4,9 @@ import { apiMessage } from './Api';
 
 // change from react component to function, in order to intergrate useState
 
-
-
 const ListeMessage = () => {
-    
+
+    // Mock data
     // const [messages, setMessages] = React.useState([{
     //     author_id: '@jean-pierre-twitter', author_name: 'Jean-Pierre', date: '11:39 AM - 29 avril 2021',
     //     text: 'Cascading Style Sheets is a style sheet language used for describing the presentation of a document written in a markup language such as HTML. CSS is a cornerstone technology of the World Wide Web, alongside HTML and JavaScript. #css', _id: "007"
@@ -18,39 +17,18 @@ const ListeMessage = () => {
 
     const [messages, setMessages] = React.useState([]);
 
-    // React.useEffect(() => {
-    //     const fetchMessages = asyn () => {
-    //         const res = await fetch 
-    //     }
-    //     fetchMessages()
-    // }, []);
-
-    // React.useEffect(() => {
-    //     const getMessages = async () => {
-    //         const fetchedMessages = await fetchMessages()
-    //         console.log(fetchedMessages)
-    //         setMessages(fetchedMessages)
-    //     }
-    //     getMessages()
-    // }, [])
-
-    // const fetchMessages = async () => {
-    //     apiMessage.get('/message').then(res => {
-    //         console.log(res.data)
-    //         return res.data
-    //     })
-    // };
-
     const fetchMessages = async () => {
         let data = await apiMessage.get('/message').then(({data}) => data);
-        // console.log(data);
-        setMessages(data);
-        console.log(messages)
+        var sorted_data = data.sort((a,b) => {
+            return new Date(a.date).getTime() - 
+                new Date(b.date).getTime()
+        }).reverse();
+        setMessages(sorted_data);
     } 
 
     React.useEffect(() => {
         fetchMessages();
-    })
+    }, [])
 
     return (
         <>
--- src/components/MessageForm.js
+++ src/components/MessageForm.js
@@ -1,36 +1,42 @@
 import React from 'react';
 import '../styles/MessageForm.css';
+import { apiMessage } from './Api';
 
-class MessageForm extends React.Component {
-    constructor(props) {
-        super(props);
-        //en props un fonction qui récupère les infos écrites
+const MessageForm = () => {
+    const [text, setText] = React.useState('')
+
+
+    const postMessage = async () => {
+        if (!text) {
+            alert('Please input text message. It can not be empty.')
+            return
+        }
+
+        let res = await apiMessage.post('/message/187239004', {
+            author_name: 'Camille', texte: text
+        })
+        console.log(res);
+        setText('');
     }
 
-    addMessage = () => {
-        console.log('Message added!')
-    }
-
-    render() {
-        return (
-            <div className='message_form_container'>
-                <div className='user_icon_col'>
-                    <div className='user_icon'></div>
+    return (
+        <div className='message_form_container'>
+            <div className='user_icon_col'>
+                <div className='user_icon'></div>
+            </div>
+            <div className='message_form_content'>
+                <div className='input_area'>
+                    <form>
+                        <textarea className='input_area_inner' placeholder="What's happening?" value={text}
+                            onChange={(e) => setText(e.target.value)}></textarea>
+                    </form>
                 </div>
-                <div className='message_form_content'>
-                    <div className='input_area'>
-                        <form>
-                            <textarea className='input_area_inner' placeholder="What's happening?"></textarea>
-                        </form>
-                    </div>
-                    <div className='toolbar_area'>
-                        <div onClick={this.addMessage} className="post_button">Tweet</div>
-                    </div>
+                <div className='toolbar_area'>
+                    <div onClick={postMessage} className="post_button">Tweet</div>
                 </div>
             </div>
-        )
-    }
-
+        </div>
+    )
 }
 
 export default MessageForm;
