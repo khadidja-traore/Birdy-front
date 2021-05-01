b67cff57e388e5b44ac15bebdb2fda6d34a1199a
Wenxuan Li <71944101+WenxuanLiGitHub@users.noreply.github.com>
2021-05-01 19:21:52 +0200

    Fixed infinite fetching of friends

--- src/components/ListeFriend.js
+++ src/components/ListeFriend.js
@@ -4,24 +4,12 @@ import { apiFriend } from './Api';
 
 // change from react component to function, in order to intergrate useState
 
-const ListeFriend = () => {
+const ListeFriend = ({ friends }) => {
     // Mock Data
     // const [friends, setFriends] = React.useState([
     //     { friend_id: '@valentine_twitter', friend_name: 'Valentine' },
     //     { friend_id: '@nathan_twitter', friend_name: 'Nathan' },
     //     { friend_id: '@lucas_twitter', friend_name: 'Lucas' }]);
-
-    const [friends, setFriends] = React.useState([]);
-
-    const fetchFriends = async () => {
-        let data = await apiFriend.get('/friends').then(({ data }) => data);
-        // console.log(data);
-        setFriends(data);
-    }
-
-    // React.useEffect(() => {
-    //     fetchFriends();
-    // }, [friends])
 
     return (
         <>
@@ -31,7 +19,6 @@ const ListeFriend = () => {
                 <Friend friend_id={12300914} friend_name={friend.secondUser}></Friend>
             ))}
         </>
-        // <button onClick={() => fetchFriends()}>Fetch</button>
     )
 }
 
--- src/pages/ProfilePage.js
+++ src/pages/ProfilePage.js
@@ -2,18 +2,46 @@ import React from 'react'
 import '../styles/ProfilePage.css';
 import ListeMessage from '../components/ListeMessage'
 import ListeFriend from '../components/ListeFriend'
-import Friend from '../components/Friend';
 import FriendForm from '../components/FriendForm';
+import { apiFriend } from '../components/Api';
 
 function ProfilePage(props) {
+    const [friend, setFriend] = React.useState('')
+    const [friends, setFriends] = React.useState([]);
+
+    const fetchFriends = async () => {
+        let data = await apiFriend.get('/friends').then(({ data }) => data);
+        setFriends(data);
+    }
+
+    const addFriend = async () => {
+        if (!friend) {
+            alert('Please input the name of the user. It can not be empty.')
+            return
+        }
+        let res = await apiFriend.post('/friends', {
+            firstUser: 'Camille', secondUser: friend
+        }).catch((e) => alert(e.message));
+        setFriend('');
+        fetchFriends();
+    }
+
     return (
         <div className="main_body" id="main_body">
             <div className="col1">
                 <div className='friend_header'>
                     Friends:
                 </div>
-                <FriendForm />
-                <ListeFriend />
+                <div className='friend_form_container'>
+                    <div className='friend_input_area'>
+                        <form>
+                            <textarea className='friend_input_area_inner' placeholder="Username" value={friend}
+                                onChange={(e) => setFriend(e.target.value)}></textarea>
+                        </form>
+                    </div>
+                    <div onClick={() => { addFriend() }} className="add_friend_button">Add Friend</div>
+                </div>
+                <ListeFriend friends={friends} />
             </div>
             <div className="col2">
                 <ListeMessage />
