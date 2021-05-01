173f668320108b12d3915e3e82becb4ad40e6da2
Wenxuan Li <71944101+WenxuanLiGitHub@users.noreply.github.com>
2021-05-01 21:52:50 +0200

    Fix delete friend by name

--- src/components/Friend.js
+++ src/components/Friend.js
@@ -12,7 +12,7 @@ const Friend = ({ deleteFriend, friend_id, friend_name}) => {
     return (
         <div className='friend_container'>
             <div className='friend_user_icon_col'>
-                <div onClick={() => deleteFriend(1)} className='friend_user_icon'></div>
+                <div onClick={() => deleteFriend(friend_name)} className='friend_user_icon'></div>
             </div>
             <div className='friend_name_col'>
                 <div className='friend_name'>{friend_name}</div>
